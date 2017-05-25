941ba0f5bf309de2e11a77179de3ea8303bb9820
Joannalq <joanna851026@hotmail.com>
2017-05-25 16:08:56 +1000

    1. create new data in our MEMES table. When doing insert and updates the SQL records need values to update the columns. We handle this using the ContentValues class. 2. using query for select the SQL records

--- app/src/main/java/com/teamtreehouse/mememaker/database/MemeDataSource.java
+++ app/src/main/java/com/teamtreehouse/mememaker/database/MemeDataSource.java
@@ -20,10 +20,86 @@ public class MemeDataSource {
         mContext=context;
         mMemeSQLiteHelper=new MemeSQLiteHelper(context);
         //force our SQLite Helper to set up the databases
-        SQLiteDatabase database=mMemeSQLiteHelper.getReadableDatabase();
+       /* SQLiteDatabase database=mMemeSQLiteHelper.getReadableDatabase();
+        database.close();*/
+    }
+
+    private SQLiteDatabase open(){
+        return mMemeSQLiteHelper.getWritableDatabase();
+    }
+
+    private void close(SQLiteDatabase database){
         database.close();
     }
 
+    //read or query option to pull back all the memes stored
+    public  ArrayList<Meme> read(){
+        return null;
+    }
+
+    public ArrayList<Meme> readMemes(){
+        SQLiteDatabase database=open();
+        //cursor for sifting through larger pieces of data
+        //select:query
+        Cursor cursor=database.query(
+                MemeSQLiteHelper.MEMES_TAB,
+                new String[]{MemeSQLiteHelper.COLUMN_MEME_NAME,BaseColumns._ID,MemeSQLiteHelper.COLUMN_MEME_ASSET},
+                null,//selection
+                null,//selection args
+                null,//group by
+                null,//having
+                null//order
+        );
+        ArrayList<Meme> memes=new ArrayList<Meme>();
+        if(cursor.moveToFirst()){
+            do{
+                Meme meme=new Meme(getIntColumName(cursor,BaseColumns._ID),
+                        getStringColumName(cursor,MemeSQLiteHelper.COLUMN_MEME_NAME),
+                        getStringColumName(cursor,MemeSQLiteHelper.COLUMN_MEME_ASSET),
+                        null);
+                memes.add(meme);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        close(database);
+        return memes;
+    }
+
+    private  int getIntColumName(Cursor cursor,String columName){
+        int columnIndex=cursor.getColumnIndex(columName);
+        return cursor.getInt(columnIndex);
+    }
+
+    private String getStringColumName(Cursor cursor,String columName){
+        int columIndex=cursor.getColumnIndex(columName);
+        return cursor.getString(columIndex);
+    }
+
+    public void create(Meme meme){
+        SQLiteDatabase database=open();
+        database.beginTransaction();
+        //implement details
+        //put actual meme model into content values
+        ContentValues memeValues=new ContentValues();
+        memeValues.put(MemeSQLiteHelper.COLUMN_MEME_NAME,meme.getName());
+        memeValues.put(MemeSQLiteHelper.COLUMN_MEME_ASSET,meme.getAssetLocation());
+        long memeID=database.insert(MemeSQLiteHelper.MEMES_TAB,null,memeValues);
+
+        for(MemeAnnotation annotation:meme.getAnnotations()){
+            ContentValues annotationVal=new ContentValues();
+            annotationVal.put(MemeSQLiteHelper.COLUMN_ANNOTATION_COLOR,annotation.getColor());
+            annotationVal.put(MemeSQLiteHelper.COLUMN_ANNOTATION_TITLE,annotation.getTitle());
+            annotationVal.put(MemeSQLiteHelper.COLUMN_ANNOTATION_X,annotation.getLocationX());
+            annotationVal.put(MemeSQLiteHelper.COLUMN_ANNOTATION_Y,annotation.getLocationY());
+            annotationVal.put(MemeSQLiteHelper.COLUMN_FOREIGN_KEY_MEME,memeID);
+
+            database.insert(MemeSQLiteHelper.ANNOTATIONS_TABLE,null,annotationVal);
+        }
+
+        database.setTransactionSuccessful();
+        database.endTransaction();
+        close(database);
+    }
 
 }
 
